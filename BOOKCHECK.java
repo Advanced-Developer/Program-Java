aba6827974dd9d434f1f1321763e767386d0cb69
Diane0780078723 <114095515+Diane0780078723@users.noreply.github.com>
2024-02-20 15:58:41 -0800

    Add files via upload

---
+++ BOOKCHECK.java
@@ -0,0 +1,59 @@
+package BOOKCHECK;
+import java.util.Scanner;
+public class BOOKCHECK{
+  
+    public static void main(String[] args) {
+    int z=1;
+    String book1;
+    String book2;
+    String book3;
+    
+       Scanner book=new Scanner(System.in);
+    for(int x=0; x<=z; x++){
+        System.out.println("Enter the number of book");
+         z=book.nextInt();
+    if(z<=3){
+   if(z==1){
+   System.out.println("Enter the name of book 1");
+   Scanner input=new Scanner (System.in);
+   book1=input.nextLine();
+   System.out.println("The book is "+book1);
+   break;
+   }
+   else if(z ==2){
+   System.out.println("Enter the name of book 1");
+   Scanner input=new Scanner (System.in);
+   book1=input.nextLine();
+   System.out.println("Enter the name of book 2");
+   Scanner inputs=new Scanner (System.in);
+           book2=inputs.nextLine();
+           System.out.println("Book one is "+book1 );
+              System.out.println( "  Book two is "+book2 );
+           break;
+   }
+   else if(z==3){
+   System.out.println("Enter the name of book 1");
+   Scanner input=new Scanner (System.in);
+   book1=input.nextLine();
+   System.out.println("Enter the name of book 2");
+   Scanner inputs=new Scanner (System.in);
+   book2=input.nextLine();
+   System.out.println("Enter the name of book 3");
+   Scanner inputa=new Scanner (System.in);
+   book3=input.nextLine();
+   System.out.println(" Book one is  "+book1);
+   System.out.println("  Book two is "+book2);
+   System.out.println(" Book three is "+book3);
+   break;
+   }}
+   else{
+   System.out.println("please try again maximum number of book is three!!");
+   
+   }
+    
+    
+    
+    }
+    }
+    
+}
